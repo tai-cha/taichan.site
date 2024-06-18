f3570d5b3cf157da6b9a50554d2fff9fb0975225
taichanne30 <dev@taichan.site>
2024-06-19 06:22:22 +0900

    Prepare for contents

---
+++ pages/docs/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-for="doc in docs">
+      <NuxtLink :to="doc._path">{{ doc.title }}</NuxtLink>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const { data: docs } = await useAsyncData('docs', () => queryContent('docs').only(['_path', 'title', 'draft']).where({ draft: false }).find())
+
+useHead({
+  title: 'ドキュメント一覧'
+})
+</script>
---
+++ pages/news/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-for="page in news">
+      <NuxtLink :to="page._path">{{ page.title }} {{ page.createdAt }}</NuxtLink>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const { data: news } = await useAsyncData('news', () => queryContent('news').only(['_path', 'title', 'createdAt', 'updatedAt', 'draft']).where({ draft: false }).find())
+
+useHead({
+  title: 'ニュース一覧'
+})
+</script>

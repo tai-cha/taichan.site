d7c7fb2855bf998ff708cf45b96413c9e30dd9fc
taichanne30 <dev@taichan.site>
2024-07-13 19:09:49 +0900

    Create Wrapper Class
    Thanks info @kakkokali-gtyih
    https://misskey.io/notes/9vnhxadab385064n

---
+++ components/content/CShareButtons.vue
@@ -0,0 +1,14 @@
+// Just a wrapper component for ~/components/ShareButtons.vue will be used in markdown components.
+<template>
+  <ShareButtons v-vind="props" />
+</template>
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  url?: string;
+  title?: string;
+  text?: string;
+  enableClipboard?: boolean;
+}>(),{
+  enableClipboard: false
+});
+</script>

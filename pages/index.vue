40bd984c750b7a41d33cfc0f717d3f3836eccd65
tai-cha <dev@taichan.site>
2024-08-22 00:27:58 +0900

    Fix: nadesskey link

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@ useSeoMeta({
       <span><Icon :class="$style.icon" name="uil:github" color="black" /><a href="https://github.com/tai-cha">GitHub (tai-cha)</a></span>
       <span><Icon :class="$style.icon" name="uil:twitter" color="#1DA1F2" /><a href="https://twitter.com/taichan_v2">Twitter (@taichan_v2)</a></span>
 
-      <span :class="$style.guideForNadesskey">もしかして: お探しのサイトは<a href="mi.taichan.site"><Icon :class="$style.icon" name="tai:nadesskey" />なですきー</a></span>
+      <span :class="$style.guideForNadesskey">もしかして: お探しのサイトは<a href="https://mi.taichan.site"><Icon :class="$style.icon" name="tai:nadesskey" />なですきー</a></span>
 
       <ShareButtons />
     </div>
